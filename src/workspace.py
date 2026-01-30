1bed005d59158dbc303fc76e20f0e67dd906082d
EugenioCos <eugenio.costagliola@icloud.com>
2026-01-30 03:34:09 +0100

    fixed  existing branch

--- src/workspace.py
+++ src/workspace.py
@@ -20,10 +20,11 @@ class Workspace:
         print(f"Workspace_path: {self.path}")
 
     def create_branch_name(self):
-        if self.settings.existing_branch is not None:
+        if self.settings.existing_branch is None:
+            random_id = ''.join(random.choices(string.digits, k=4))
+            self.branch_name = f"{self.settings.job_name}_{random_id}"
+        else:
             self.branch_name = self.settings.existing_branch
-        random_id = ''.join(random.choices(string.digits, k=4))
-        self.branch_name = f"{self.settings.job_name}_{random_id}"
 
     def create_branch(self, project_path):
         git_path = os.path.join(self.path, ".git")
@@ -32,7 +33,8 @@ class Workspace:
             shutil.copytree(project_git_path, git_path)
         self.repo = Repo(git_path)
         self.git_cmd = self.repo.git
-        self.git_cmd.checkout("HEAD", b=self.branch_name)  # Create a new branch.
+        if self.settings.existing_branch is None: 
+            self.git_cmd.checkout("HEAD", b=self.branch_name)  # Create a new branch.
         
     def init_workspace(self, project_path):
         self.path = self.settings.workspace_path + self.branch_name
@@ -45,9 +47,10 @@ class Workspace:
 
     def commit(self, commit_message: str) -> bool:
         self.repo.index.add(self.files)
-        if not self.git_cmd.diff("--cached", "--name-only"):
+        if '.' not in self.git_cmd.diff("--cached", "--name-only"):
             return False
         self.repo.index.commit(commit_message)
+        print("Commit done")
         return True
 
                 
