a9868caa3c946f5db365a235b1f9be9dd9ddb02a
EugenioCos <eugenio.costagliola@icloud.com>
2026-01-30 16:12:05 +0100

    update

--- src/data/settings.py
+++ src/data/settings.py
@@ -4,12 +4,15 @@ class Settings:
 
     def __init__(self, settings_path):
         try:
-            self.json = json.load(open(settings_path))
+            with open(settings_path, 'r') as f:
+                self.json = json.load(f)
             self.job_name = self.json["job_name"]
             self.model = self.json["model"]
             self.branch = self.json["branch"]
             self.response_path = self.json["response_path"]
             self.ignore_files = self.json["ignore_files"]
             self.workspace_path = self.json["workspace_path"]
-        except:
-            raise Exception("invalid settings")
+        except FileNotFoundError:
+            raise Exception("Settings file not found: {0}".format(settings_path))
+        except json.JSONDecodeError:
+            raise Exception("Invalid JSON in settings file: {0}".format(settings_path))
